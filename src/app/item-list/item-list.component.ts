99cd20b8bff997484110f919a7ba8016b52f42b3
durkoi <istvan.durko@gmail.com>
2022-08-07 13:29:10 +0200

    commit all initial changes, list w angular, bootstrap included, works OK.

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {ItemService} from "../services/item.service";
+import {IItem} from "../model/IItem";
 
 @Component({
   selector: 'app-item-list',
-  templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.css']
+  templateUrl: './item-list.component.html'
 })
 export class ItemListComponent implements OnInit {
 
-  constructor() { }
+  public items: Array<IItem>;
+  public errorMsg: string;
+
+  constructor(private itemService: ItemService) {
+    this.items = [];
+    this.errorMsg = '';
+  }
 
   ngOnInit(): void {
+    this.itemService.getItems().subscribe(data => this.items = data);
   }
 
 }
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IItem} from "../model/IItem";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  private url: string = '/assets/data/items.json'
+
+  constructor(private http: HttpClient) {
+  }
+
+  getItems(): Observable<IItem[]> {
+    return this.http.get<IItem[]>(this.url);
+  }
 }
